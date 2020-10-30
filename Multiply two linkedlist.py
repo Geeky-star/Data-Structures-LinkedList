c8343c4482fdd3409a40ebb3412551c81610648f
Geeky-Star <nimratkaur540@gmail.com>
2020-10-31 04:44:10 +0530

    Create Multiply two linkedlist.py

---
+++ Multiply two linkedlist.py
@@ -0,0 +1,16 @@
+def multiplyTwoList(head1, head2):
+    num1 = 0
+    num2 = 0
+    
+    while head1:
+        num1=num1*10+head1.data
+        head1=head1.next
+        
+    while head2:
+        num2 = num2*10+head2.data
+        head2=head2.next
+        
+        
+    return (num1*num2)%(10**9+7)
+
+
